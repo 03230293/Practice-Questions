93ffa7f31fd008d0e7697f797df456b8fe687267
03230293 <03230293.gcbs@rub.edu.bt>
2024-04-11 19:47:44 +0600

    input

---
+++ 11_4_24.py
@@ -0,0 +1,12 @@
+One=(input(" One number: "))
+print(One)
+two=(input(" One number: "))
+print(two)
+
+if One >= two:
+    if One > two:
+        print("One is greater")
+    else: 
+        print("equal to two") 
+else:
+    print("Two is greater")
